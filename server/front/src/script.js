86892b15146185e73cea318e76115d20ac6b5705
PlatiScript <simonbultel@hotmail.fr>
2021-04-21 15:22:43 +0200

    Update Front

--- server/front/src/script.js
+++ server/front/src/script.js
@@ -1,30 +1,59 @@
 import './style.css';
+
 const axios = require('axios').default;
+axios.interceptors.request.use( x => {
+  // to avoid overwriting if another interceptor
+  // already defined the same object (meta)
+  x.meta = x.meta || {}
+  x.meta.requestStartedAt = new Date().getTime();
+  return x;
+})
+
+axios.interceptors.response.use( x => {
+
+  document.getElementById("resultInfos").innerHTML=  " ("+((new Date().getTime() - x.config.meta.requestStartedAt)/1000+ " secondes)");
+      return x;
+  },
+  // Handle 4xx & 5xx responses
+  x => {
+    document.getElementById("resultInfos").innerHTML=  ((new Date().getTime() - x.config.meta.requestStartedAt)/1000+ " secondes");
+    throw x;
+  }
+)
+
+
 
 document.getElementById("resultSearch").style.display = "none"
 
+
 document.getElementById("submitSearch").onclick = function(){
-
-
-
 
   let searchValue = document.getElementById("searchInput").value
 
-
   axios.get('http://localhost:4000?query='+searchValue)
     .then(function (response) {
+
       if(searchValue == "" || searchValue == null)
-      return;
-  
-      document.getElementById("resultInfos").innerHTML=  response.hits.hits.length+" résultats ("+response.took/1000+ " secondes)"
-      document.getElementById("results").innerHTML ="";
-      response.hits.hits.forEach(result => {
+        return;
+
         document.getElementById("resultSearch").style.display = "block"      
+
+      document.getElementById("resultInfos").innerHTML=  response.data.hits.hits.length+" résultats" + document.getElementById("resultInfos").innerHTML;
+
+      if(response.data.hits.hits.length <= 0){
+        document.getElementById("resultInfos").innerHTML +="<h3 class='text-center'>Aucun résultat pour la recherche : "+searchValue+"</h3>"
+      }
+
+      document.getElementById("results").innerHTML ="";
+      response.data.hits.hits.forEach(result => {
         let indexOfResult = result._source.content.indexOf(searchValue);
-        let content = result._source.content.substr(indexOfResult-130, 230).replace(new RegExp(searchValue,"g"), function (x) {
+        let content = result._source.content.substr(indexOfResult-20, 230).replace(new RegExp(searchValue,"g"), function (x) {
           return "<span class='find'>"+x+"</span>";
         }) + "...";
-        document.getElementById("results").innerHTML += '<div class="result"><hr><h3>'+result._source.title+' <span class="author">par '+result._source.author+'</span> <span class="type">'+result._type+'</span></h3><p>'+content+'</p><a href="#" class="btn">Voir le fichier</a></div>';
+        if(result._source.author == null){
+          result._source.author ="Inconnu"
+        }
+        document.getElementById("results").innerHTML += '<div class="result"><hr><h3>'+result._source.path.split(".")[0]+' <span class="author">par '+result._source.author+'</span> <span class="type">'+result._source.path.split(".")[1].toUpperCase()+'</span></h3><p>'+content+'</p><a href="#" class="btn">Voir le fichier</a></div>';
       })
       
     })
@@ -35,7 +64,5 @@ document.getElementById("submitSearch").onclick = function(){
     .then(function () {
 
     });
-
-
 
 }
