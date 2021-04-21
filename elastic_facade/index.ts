a32462e587663cbc470f9e123d2f0ef5498dbc47
Khornya <marine.khornya@gmail.com>
2021-04-21 13:35:32 +0200

    ✨ add documents to elastic + perform search on document content

--- elastic_facade/index.ts
+++ elastic_facade/index.ts
@@ -25,11 +25,11 @@ server.route({
   handler: async function (request, reply) {
     const query = (request.query as any)["query"]
     const { body } = await client.search({
-      index: 'game-of-thrones',
+      index: 'documents',
       body: {
         query: {
           match: {
-            quote: query
+            content: query
           }
         }
       }
@@ -49,42 +49,10 @@ server.listen(8080, '0.0.0.0', (err) => {
 });
 
 //ElasticSearch client
-export const client = new Client({
+const client = new Client({
   node: 'http://elastic:9200',
   auth: {
     username: process.env.ELASTICSEARCH_USER || "",
     password: process.env.ELASTICSEARCH_PASSWORD || ""
   }
 })
-
-
-async function initData() {
-  await client.index({
-    index: 'game-of-thrones',
-    refresh: true,
-    body: {
-      character: 'Ned Stark',
-      quote: 'Winter is coming.'
-    }
-  })
-
-  await client.index({
-    index: 'game-of-thrones',
-    refresh: true,
-    body: {
-      character: 'Daenerys Targaryen',
-      quote: 'I am the blood of the dragon.'
-    }
-  })
-
-  await client.index({
-    index: 'game-of-thrones',
-    refresh: true,
-    body: {
-      character: 'Tyrion Lannister',
-      quote: 'A mind needs books like a sword needs a whetstone.'
-    }
-  })
-}
-
-initData().catch(console.log)
