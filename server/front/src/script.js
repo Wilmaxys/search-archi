95d5d8dd2e51e61d69e4115c4873301e59e9737d
PlatiScript <simonbultel@hotmail.fr>
2021-04-21 14:31:46 +0200

    update front + add cors to api

--- server/front/src/script.js
+++ server/front/src/script.js
@@ -1,26 +1,41 @@
 import './style.css';
 const axios = require('axios').default;
 
-document.getElementById('submitSearch').onclick = function () {
-  var json = require('./test.json');
-  document.getElementById('resultInfos').innerHTML =
-    json.hits.hits.length + ' résultats (' + json.took / 1000 + ' secondes)';
-  document.getElementById('results').innerHTML = '';
-  json.hits.hits.forEach((response) => {
-    document.getElementById('results').innerHTML +=
-      '<div class="result"><hr><h3>1-Guerre de Canudos.pdf</h3><p>Le fondateur de ladite communauté, Antônio Conselhei-ro,prophète millénariste <span>ambulant</span>, prêchait une moraled’abstinence et considérait la...</p></div>';
-  });
+document.getElementById("resultSearch").style.display = "none"
 
-  axios
-    .get('http://localhost:8081/test')
+document.getElementById("submitSearch").onclick = function(){
+
+
+
+
+  let searchValue = document.getElementById("searchInput").value
+
+
+  axios.get('http://localhost:4000?query='+searchValue)
     .then(function (response) {
-      // handle success
+      if(searchValue == "" || searchValue == null)
+      return;
+  
+      document.getElementById("resultInfos").innerHTML=  response.hits.hits.length+" résultats ("+response.took/1000+ " secondes)"
+      document.getElementById("results").innerHTML ="";
+      response.hits.hits.forEach(result => {
+        document.getElementById("resultSearch").style.display = "block"      
+        let indexOfResult = result._source.content.indexOf(searchValue);
+        let content = result._source.content.substr(indexOfResult-130, 230).replace(new RegExp(searchValue,"g"), function (x) {
+          return "<span class='find'>"+x+"</span>";
+        }) + "...";
+        document.getElementById("results").innerHTML += '<div class="result"><hr><h3>'+result._source.title+' <span class="author">par '+result._source.author+'</span> <span class="type">'+result._type+'</span></h3><p>'+content+'</p><a href="#" class="btn">Voir le fichier</a></div>';
+      })
+      
     })
     .catch(function (error) {
-      // handle error
+
       console.log(error);
     })
     .then(function () {
-      // always executed
+
     });
-};
+
+
+
+}
