0e944c84e05f2e4633c074a303f69f590367b4b4
PlatiScript <simonbultel@hotmail.fr>
2021-04-22 11:16:22 +0200

    fix front

--- server/front/src/script.js
+++ server/front/src/script.js
@@ -1,6 +1,34 @@
 import './style.css';
 
+
+
+function download(name){
+  axios.get("http://localhost:4002/"+name,
+        {
+            responseType: 'arraybuffer',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/pdf'
+            }
+        })
+        .then((response) => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', name); //or any other extension
+            document.body.appendChild(link);
+            link.click();
+        })
+        .catch((error) => console.log(error));
+
+
+  }
+
 const axios = require('axios').default;
+axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.baseURL = 'http://localhost:8080';
+
 axios.interceptors.request.use((x) => {
   // to avoid overwriting if another interceptor
   // already defined the same object (meta)
@@ -26,24 +54,28 @@ axios.interceptors.response.use(
   }
 );
 
+
+
 document.getElementById('resultSearch').style.display = 'none';
 
 document.getElementById('submitSearch').onclick = function () {
+
   let searchValue = document.getElementById('searchInput').value;
 
   axios
     .get('http://localhost:4000?query=' + searchValue)
     .then(function (response) {
+
       if (searchValue == '' || searchValue == null) return;
 
       document.getElementById('resultSearch').style.display = 'block';
 
       document.getElementById('resultInfos').innerHTML =
-        response.data.hits.hits.length +
+        response.data.length +
         ' résultats' +
         document.getElementById('resultInfos').innerHTML;
 
-      if (response.data.hits.hits.length <= 0) {
+      if (response.data.length <= 0) {
         document.getElementById('resultInfos').innerHTML +=
           "<h3 class='text-center'>Aucun résultat pour la recherche : " +
           searchValue +
@@ -51,31 +83,40 @@ document.getElementById('submitSearch').onclick = function () {
       }
 
       document.getElementById('results').innerHTML = '';
-      response.data.hits.hits.forEach((result) => {
-        let indexOfResult = result._source.content.indexOf(searchValue);
-        let content =
-          result._source.content
-            .substr(indexOfResult - 20, 230)
-            .replace(new RegExp(searchValue, 'g'), function (x) {
-              return "<span class='find'>" + x + '</span>';
-            }) + '...';
-        if (result._source.author == null) {
-          result._source.author = 'Inconnu';
+
+      var id = 0;
+
+      response.data.forEach((result) => {
+        
+        if (result.author == null) {
+          result.author = 'Inconnu';
         }
-        document.getElementById('results').innerHTML +=
-          '<div class="result"><hr><h3>' +
-          result._source.path.split('.')[0] +
-          ' <span class="author">par ' +
-          result._source.author +
-          '</span> <span class="type">' +
-          result._source.path.split('.')[1].toUpperCase() +
-          '</span></h3><p>' +
-          content +
-          '</p><a href="#" class="btn">Voir le fichier</a></div>';
-      });
+        var extension = result.path.split(".")[result.path.split(".").length-1].toUpperCase();
+        var title = result.path.split("/")[result.path.split("/").length-1].replace('.'+extension.toLowerCase(), '');
+
+        document.getElementById("results").innerHTML += '<div class="result" id="result_'+id+'"><hr><h3>'+title+' <span class="author">par '+result.author+'</span> <span class="type">'+extension+'</span></h3><p>'+result.content+'</p></div>';
+        
+      
+        var a = document.createElement('a');
+        var linkText = document.createTextNode("Télécharger le fichier");
+        a.appendChild(linkText);
+        a.addEventListener('click', function(){
+          download(result.path.split("/")[result.path.split("/").length-1]);
+        })
+        a.className += "btn";
+
+
+        document.getElementById("result_"+id).append(a);
+
+      
+        id++;
+      })
+      
+
     })
     .catch(function (error) {
       console.log(error);
     })
     .then(function () {});
 };
+
